714c9187a43ff7e2e4504463f57eefece7d85657
Kyler Swanson <kswans02@uoguelph.ca>
2022-09-28 14:54:28 -0400

    Add json-to-csv test suite
    Co-authored-by: Andrew Heft <aheft@uoguelph.ca>

---
+++ tests/test_jsontocsv.py
@@ -0,0 +1,23 @@
+import sys
+import unittest
+import filecmp
+
+sys.path.append('../jsontocsv')
+from src.jsontocsv.core import JSONToCSV
+
+class JSONToCSVTests(unittest.TestCase):
+    def setUp(self):
+        self.jsontocsv = JSONToCSV('tests/data/results.json')
+
+    def test_generate_sections(self):
+        self.jsontocsv.createSectionsCSV('tests/data/csv/sections.csv')
+
+        self.assertTrue(filecmp.cmp('tests/data/csv/example_sections.csv', 'tests/data/csv/sections.csv'))
+
+    def test_generate_meetings(self):
+        self.jsontocsv.createMeetingsCSV('tests/data/csv/meetings.csv')
+    
+        self.assertTrue(filecmp.cmp('tests/data/csv/example_meetings.csv', 'tests/data/csv/meetings.csv'))
+
+if __name__ == "__main__":
+    unittest.main()
