0c538b92a9b1e64672bbd48b726c2137d3078c54
Kyler Swanson <kswans02@uoguelph.ca>
2022-10-01 17:41:10 -0400

    Move suggestion flags to sections.csv

--- src/jsontocsv/core.py
+++ src/jsontocsv/core.py
@@ -11,37 +11,31 @@ class JSONToCSV:
         for course_code, sections in self.courses.items():
             for section in sections:
                 for i in range(len(section['meeting'])):
-                    meeting = section['meeting'][i]
-
-                    if section['faculty'] == 'G. Klotz':
-                        meeting['professorGreg'] = 1
-                    else:
-                        meeting['professorGreg'] = 0
-
                     self.meetings.append(section['meeting'][i])
 
     def createSectionsCSV(self, output_file):
         with open(output_file, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter='=', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(['Course Code', 'Section', 'Term', 'Status', 'Course Name', 'Location', 'Faculty', 'Available', 'Capacity', 'Credits', 'Level', 'Meetings'])
+            writer.writerow(['Course Code', 'Section', 'Term', 'Status', 'Course Name', 'Location', 'Faculty', 'Available', 'Capacity', 'Credits', 'Level', 'Meetings', 'isMorning', 'isAfternoon', 'isEvening', 'onFriday', 'onThursday', 'professorGreg'])
 
             curr_meeting_id = 0
             for course_code, sections in self.courses.items():
                 for section in sections:
                     meeting_ids = []
 
-                    for i in range(len(section['meeting'])):
+                    for meeting in section['meeting']:
                         meeting_ids.append(str(curr_meeting_id))
                         curr_meeting_id += 1
 
-                    output_row = [str(course_code), section['section'], section['term'], section['status'], section['courseName'], section['location'], section['faculty'], (section['available'] if 'available' in section else ''), (section['capacity'] if 'capacity' in section else ''), section['credits'], section['academicLevel'], '-'.join(meeting_ids)]
-                    writer.writerow(output_row)
+                    flags = self.getSuggestionFlags(section['meeting'])
 
+                    output_row = [str(course_code), section['section'], section['term'], section['status'], section['courseName'], section['location'], section['faculty'], (section['available'] if 'available' in section else ''), (section['capacity'] if 'capacity' in section else ''), section['credits'], section['academicLevel'], '-'.join(meeting_ids), flags['isMorning'], flags['isAfternoon'], flags['isEvening'], flags['onFriday'], flags['onThursday'], 1 if section['faculty'] == 'G. Klotz' else 0]
+                    writer.writerow(output_row)
 
     def createMeetingsCSV(self, output_file):
         with open(output_file, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter='=', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(['id', 'Meeting Type', 'Meeting Day', 'Start Time', 'End Time', 'Building', 'Room', 'Exam Date', 'isMorning', 'isAfternoon', 'isEvening', 'onFriday', 'onThursday', 'professorGreg'])
+            writer.writerow(['id', 'Meeting Type', 'Meeting Day', 'Start Time', 'End Time', 'Building', 'Room', 'Exam Date'])
 
             for i in range(len(self.meetings)):
                 meeting = self.meetings[i]
@@ -53,19 +47,7 @@ class JSONToCSV:
 
                     meeting['end_time'] = end_time[0:end_time.find("(")].strip()
 
-                # suggestion flags
-
-                start_time = None
-                if meeting['start_time'] != 'Times TBA':
-                    start_time = datetime.strptime(meeting['start_time'], '%I:%M%p')
-
-                isMorning = 1 if start_time and start_time.hour < 12 else 0
-                isAfternoon = 1 if start_time and start_time.hour >= 12 and start_time.hour < 17 else 0
-                isEvening = 1 if start_time and start_time.hour >= 17 else 0
-                onFriday = 1 if start_time and 'Fri' in meeting['meeting_day'] else 0
-                onThursday = 1 if start_time and 'Thur' in meeting['meeting_day'] else 0
-
-                output_row = [str(i), meeting['meeting_type'], meeting['meeting_day'], meeting['start_time'], meeting['end_time'], meeting['building'], meeting['room'], exam_date, isMorning, isAfternoon, isEvening, onFriday, onThursday, meeting['professorGreg']]
+                output_row = [str(i), meeting['meeting_type'], meeting['meeting_day'], meeting['start_time'], meeting['end_time'], meeting['building'], meeting['room'], exam_date]
                 writer.writerow(output_row)
     
     def getCourseData(self, json_file):
@@ -74,3 +56,26 @@ class JSONToCSV:
         file.close()
 
         return courses
+
+    def getSuggestionFlags(self, meetings):
+        out = {
+            'isMorning': 0,
+            'isAfternoon': 0,
+            'isEvening': 0,
+            'onFriday': 0,
+            'onThursday': 0
+        }
+
+        for meeting in meetings:
+            if meeting['start_time'] == 'Times TBA':
+                continue
+            
+            start_time = datetime.strptime(meeting['start_time'], '%I:%M%p')
+
+            out['isMorning'] = 1 if start_time and start_time.hour < 12 else out['isMorning']
+            out['isAfternoon'] = 1 if start_time and start_time.hour >= 12 and start_time.hour < 17 else out['isAfternoon']
+            out['isEvening'] = 1 if start_time and start_time.hour >= 17 else out['isEvening']
+            out['onFriday'] = 1 if start_time and 'Fri' in meeting['meeting_day'] else out['onFriday']
+            out['onThursday'] = 1 if start_time and 'Thur' in meeting['meeting_day'] else out['onThursday']
+            
+        return out
