105f03ee2aa815bcdd37473c57bd459d0c32b11d
Kyler Swanson <kswans02@uoguelph.ca>
2022-10-20 20:28:52 -0400

    Add Kubernetes deployments and services + update Dockerfiles and NGINX

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -12,3 +13,6 @@ def get_response():
 def post_response(coursecode):
     data_list.append(coursecode)
     return jsonify(data_list)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
