503206034e3686e8e2368b5b132735e798d08cf5
Kyler Swanson <kswans02@uoguelph.ca>
2022-10-31 10:26:27 -0400

    Add Docker volumes for hot reload

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,41 @@
-import React, { useState } from "react";
-import axios from "axios"
-import logo from "./logo.png";
-import myImage from "../src/timetable.png";
-import "./App.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import logo from './logo.png';
+import myImage from '../src/timetable.png';
+import './App.css';
 
 function App() {
-  const [courses,setResult] = useState("")
-  const [course,setCourse] = useState("")
+  const [courses, setResult] = useState('');
+  const [course, setCourse] = useState('');
   const testapi = () => {
-    axios.post("/api/searchcode/"+course).then((response) => {
-          setResult(JSON.stringify(response.data))
+    axios.post('/api/searchcode/' + course).then((response) => {
+      setResult(JSON.stringify(response.data));
     });
-  }
-  
-  
+  };
+
   return (
-    <div className="App">
-      <div className="row">
-        <div className="col">
-          <img src={logo} className="img-fluid" alt="logo" />
+    <div className='App'>
+      <div className='row'>
+        <div className='col'>
+          <img src={logo} className='img-fluid' alt='logo' />
           <br></br>
-          <nav className="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
+          <nav className='navbar navbar-expand-lg bg-light'>
+            <div className='container-fluid'>
               <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
+                className='navbar-toggler'
+                type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#navbarNav'
+                aria-controls='navbarNav'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
               >
-                <span class="navbar-toggler-icon"></span>
+                <span className='navbar-toggler-icon'></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <a
-                      className="nav-link active fs-3 mr-3"
-                      aria-current="page"
-                      href="#home"
-                    >
+              <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav'>
+                  <li className='nav-item'>
+                    <a className='nav-link active fs-3 mr-3' aria-current='page' href='#home'>
                       Home
                     </a>
                   </li>
@@ -50,134 +45,87 @@ function App() {
           </nav>
         </div>
       </div>
-      <div className="row">
-        <div className="col-xl">
-          <div className="mt-4 ms-4 px-5 py-4 rounded-4 courses">
-            <div className="fs-3 mt-4">Enter Desired Courses</div>
-            <form className="mt-2">
-              <div className="mb-3">
-                <input
-                  id="course1"
-                  className="form-control"
-                  type="text"
-                  placeholder=" "
-                  value={course}
-                  onChange={e => setCourse(e.target.value)}
-                />
-                <label for="course1" className="form-label">
+      <div className='row'>
+        <div className='col-xl'>
+          <div className='mt-4 ms-4 px-5 py-4 rounded-4 courses'>
+            <div className='fs-3 mt-4'>Enter Desired Courses</div>
+            <form className='mt-2'>
+              <div className='mb-3'>
+                <input id='course1' className='form-control' type='text' placeholder=' ' value={course} onChange={(e) => setCourse(e.target.value)} />
+                <label for='course1' className='form-label'>
                   Enter Course
                 </label>
               </div>
             </form>
-            <div className="text-center">
-              <button type="button" className="btn btn-secondary" onClick={testapi}>
+            <div className='text-center'>
+              <button type='button' className='btn btn-secondary' onClick={testapi}>
                 Find Course
               </button>
             </div>
             <br></br>
-            <div className="text-center">
-              <button type="button" className="btn btn-secondary">
-               Generate Schedule
+            <div className='text-center'>
+              <button type='button' className='btn btn-secondary'>
+                Generate Schedule
               </button>
             </div>
           </div>
           <pre>{courses}</pre>
         </div>
-        <div className="col-xl">
-          <div className="checkboxes mt-4 ms-5">
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box1"
-              ></input>
-              <label className="form-check-label" for="box1">
+        <div className='col-xl'>
+          <div className='checkboxes mt-4 ms-5'>
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box1'></input>
+              <label className='form-check-label' for='box1'>
                 Prefer Heavy Mornings
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box2"
-              ></input>
-              <label className="form-check-label" for="box2">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box2'></input>
+              <label className='form-check-label' for='box2'>
                 Prefer Heavy Afternoons
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box3"
-              ></input>
-              <label className="form-check-label" for="box3">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box3'></input>
+              <label className='form-check-label' for='box3'>
                 Prefer Heavy Evenings
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box4"
-              ></input>
-              <label className="form-check-label" for="box4">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box4'></input>
+              <label className='form-check-label' for='box4'>
                 Avoid Thursdays
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box5"
-              ></input>
-              <label className="form-check-label" for="box5">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box5'></input>
+              <label className='form-check-label' for='box5'>
                 Avoid Fridays
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box6"
-              ></input>
-              <label className="form-check-label" for="box6">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box6'></input>
+              <label className='form-check-label' for='box6'>
                 Prefer courses with Dr. Greg
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box7"
-              ></input>
-              <label className="form-check-label" for="box7">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box7'></input>
+              <label className='form-check-label' for='box7'>
                 Allow Graduate courses
               </label>
             </div>
-            <div className="form-check mb-4">
-              <input
-                type="checkbox"
-                className="form-check-input mt-0"
-                value=""
-                id="box8"
-              ></input>
-              <label className="form-check-label" for="box8">
+            <div className='form-check mb-4'>
+              <input type='checkbox' className='form-check-input mt-0' value='' id='box8'></input>
+              <label className='form-check-label' for='box8'>
                 Allow Courses with TBA times
               </label>
             </div>
           </div>
         </div>
-        <div className="col-xl">
-          <div className="mt-4 me-4">
-            <img src={myImage} className="img-fluid" alt="logo" />
+        <div className='col-xl'>
+          <div className='mt-4 me-4'>
+            <img src={myImage} className='img-fluid' alt='logo' />
           </div>
         </div>
       </div>
--- server/app.py
+++ server/app.py
@@ -17,4 +17,3 @@ def post_searchcode(coursecode):
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3001)))
-    
