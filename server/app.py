650c7012d53b4b1b91972933b77c954f859f71c5
Alireza Sharif <ashari01@uoguelph.ca>
2022-11-02 20:50:41 -0400

    added partial course code + multiple course code seperated with space features

--- server/app.py
+++ server/app.py
@@ -16,10 +16,17 @@ def get_searchcode():
     args = request.args
     courseCode = args.get('q')
 
-    if courses.get(courseCode.upper()) is not None:
-        return jsonify(courses.get(courseCode.upper()))
+    # if courses.get(courseCode.upper()) is not None:
+    #     return jsonify(courses.get(courseCode.upper()))
 
-    return jsonify([])
+    # return jsonify([])
+    courseCodes = courseCode.split()
+    resp = []
+    for code in courseCodes:
+
+        resp.append(dict(filter(lambda item: code in item[0], courses.items())))
+    print(resp)
+    return jsonify(resp)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=PORT)
