88c2bc3bdca9e6c81ef74f5900b67ab4cb9a4fb8
Robert Stegmann <rstegman@uoguelph.ca>
2022-11-07 10:30:27 -0500

    added faculty and multisearch

--- server/app.py
+++ server/app.py
@@ -28,28 +28,40 @@ def get_search():
 
 def search(query, data):
     # treat * as space
-    query = query.replace("*", " ")
-    query = query.upper()
-    terms = query.split()
+    
+    queries = query.split(";")
+    queryResults = []
+    for q in queries:
+        if q.strip() != "":
+            tempQuery = q.replace("*", " ")
+            tempQuery = tempQuery.upper()
+            terms = tempQuery.split()
 
-    out = data
-    for term in terms:
-        # every time only search from the courses that have been chosen by the last term
-        matches = []
-        for course in out:
-            if term in course['course'] or term in course['courseName'].upper():
-                matches.append(course)
-            else:
-                sections = course['sections']
-                for section in sections:
-                    sectionId = section['section'].strip()
-                    if term in sectionId:
+            out = data
+            for term in terms:
+                # every time only search from the courses that have been chosen by the last term
+                matches = []
+                for course in out:
+                    if term in course['course'] or term in course['courseName'].upper():
                         matches.append(course)
-                        break
+                    else:
+                        sections = course['sections']
+                        for section in sections:
+                            sectionId = section['section'].strip()
+                            if term in sectionId:
+                                matches.append(course)
+                                break
+                            elif term in section['faculty'].strip().upper():
+                                matches.append(course)
+                                break
 
-        out = matches
-
-    return out
+                out = matches
+            
+            queryResults.extend(out)
+            #queryResults = list(set(queryResults) | set(out))
+            
+            
+    return queryResults
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=PORT)
