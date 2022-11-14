969503566c1eee654b8424067cdf4288cc7e9d3b
Kyler Swanson <kswans02@uoguelph.ca>
2022-11-13 19:14:48 -0500

    Update E2E tests to use Chrome and change container (#8)
    * Update E2E tests to use Chrome and change container
    
    * Move Cypress tests to own container
    
    * Remove container on test workflow
    
    * Move build to one line
    
    * Update container image and dev dep
    
    * Change Cypress image
    
    * Update base URL and video dir
    
    * Add wait-on to e2e
    
    * Call wait-on with npx
    
    * Change wait-on endpoint
    
    * Add docker-compose health check
    
    * Change base url
    
    * Change cy endpoint
    
    * Change CRA test host
    
    * Enable DANGEROUSLY_DISABLE_HOST_CHECK

--- e2e/cypress/e2e/Tests.cy.js
+++ e2e/cypress/e2e/Tests.cy.js
@@ -1,12 +1,12 @@
 describe('Check the liveness of the server', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
   });
 });
 
 describe('Search a course and check the correct value', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
     cy.get('input.form-control').type('cis3760');
     cy.xpath('//*[@id="results-accordion"]/div/h2/button'); //xpath for course result
     cy.contains('CIS*3760 - Software Engineering');
@@ -15,7 +15,7 @@ describe('Search a course and check the correct value', () => {
 
 describe('Add a section of a course to schedule and check the timing is correct', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
     cy.get('input.form-control').type('cis3760');
     cy.xpath('//*[@id="results-accordion"]/div/h2/button').click(); //xpath for course result
     cy.xpath('//*[@id="collapse-225"]/div/div/li[1]/h5/button').click(); //xpath for section
@@ -26,7 +26,7 @@ describe('Add a section of a course to schedule and check the timing is correct'
 
 describe('Add a section, check the timing and then remove the section', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
     cy.get('input.form-control').type('cis3760');
     cy.xpath('//*[@id="results-accordion"]/div/h2/button').click(); //xpath for course result
     cy.xpath('//*[@id="collapse-225"]/div/div/li[1]/h5/button').click(); //xpath for section
@@ -39,7 +39,7 @@ describe('Add a section, check the timing and then remove the section', () => {
 
 describe('Add 2 sections of 2 different courses', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
     cy.get('input.form-control').type('cis3760');
     cy.xpath('//*[@id="results-accordion"]/div/h2/button').click(); //xpath for course result
     cy.xpath('//*[@id="collapse-225"]/div/div/li[1]/h5/button').click(); //xpath for section
@@ -57,7 +57,7 @@ describe('Add 2 sections of 2 different courses', () => {
 
 describe('Add a section and check if the final exam was added correctly', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://app:3000/');
     cy.get('input.form-control').type('acct1220');
     cy.xpath('//*[@id="results-accordion"]/div/h2/button').click(); //xpath for course result
     cy.xpath('//*[@id="collapse-1"]/div/div/li[1]/h5/button/i').click(); //xpath for section
