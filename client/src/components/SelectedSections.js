6be2fc9a7c8b5d6122399e565094fa3a06875483
Kyler Swanson <kylerswanson@gmail.com>
2022-11-17 15:09:50 -0500

    Refactor htmlparser to generate .sql and create DB seeding script (#20)
    * created the majority of code to generate sql in python
    
    * Fix dictionary keys and exam date/time
    
    * Add section number to SelectedSections
    
    * Remove trailing whitespace
    
    * Update comment
    
    * Fix incorrect pks on sections
    
    * Update sql generation logic and create seed_db script
    
    
    Co-authored-by: Alireza Sharif<alirezasharif99@gmail.com>
    Co-authored-by: Mackenzie Kean <mkean01@uoguelph.ca>

--- client/src/components/SelectedSections.js
+++ client/src/components/SelectedSections.js
@@ -10,7 +10,7 @@ const SelectedSections = () => {
       {schedule.map((section) => (
         <li key={section.num} className='list-group-item p-3'>
           <h5>
-            {section.department}*{section.courseCode} {section.courseName}
+            {section.department}*{section.courseCode} {section.courseName} - {section.section}
             <SectionToggle section={section} />
           </h5>
         </li>
