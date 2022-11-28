9761533164a8f2915d4a068605252b4cb456d27b
Mackenzie Kean <mkean01@uoguelph.ca>
2022-11-28 01:30:10 -0500

    added CSS variables and new state for dark and light modes storing preference on local storage.  Basic dark mode and light mode implemented sucessfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import logo from './logo5.png';
-import './App.css';
-import Schedule from './components/Schedule';
-import { ScheduleContextProvider } from './contexts/ScheduleContext';
-import ExamSchedule from './components/ExamSchedule';
-import SemesterSelector from './components/SemesterSelector';
-import Search from './components/Search';
+import React, { useState } from "react";
+import logo from "./logo5.png";
+import "./App.css";
+import Schedule from "./components/Schedule";
+import { ScheduleContextProvider } from "./contexts/ScheduleContext";
+import ExamSchedule from "./components/ExamSchedule";
+import SemesterSelector from "./components/SemesterSelector";
+import Search from "./components/Search";
 
 function App() {
   const [examView, setExamView] = useState(false);
 
+  //const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  // const [theme, setTheme] = useLocalStorage(
+  //   "theme",
+  //   defaultDark ? "dark" : "light"
+  // );
+  let storedTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(
+    storedTheme === null ? "Dark" : storedTheme
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "Light" ? "Dark" : "Light";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+
+  //useEffect(() => {}, [theme]);
+
   return (
     <ScheduleContextProvider>
-      <div className='App'>
-        <div className='d-flex flex-column h-100'>
-          <a href='#home'>
-            <img src={logo} className='mt-3 img-fluid' alt='logo' />
+      <div className="App container-fluid" data-theme={theme}>
+        <div className="d-flex flex-column h-100">
+          <a href="#home">
+            <img src={logo} className="mt-3 img-fluid" alt="logo" />
           </a>
-          <div className='container-fluid flex-grow-1'>
-            <div className='row p-3'>
-              <div className='col-xl-4 col-xxl-3'>
+          <div className="container-fluid flex-grow-1">
+            <div className="row p-3">
+              <div className="col-xl-4 col-xxl-3">
                 <Search />
               </div>
 
-              <div className='col-xl-8 col-xxl-9 flex-row-1'>
-                <SemesterSelector />
+              <div className="col-xl-8 col-xxl-9 flex-row-1">
+                <SemesterSelector themeFunction={switchTheme} theme={theme} />
 
-                <div className='mt-4'>
+                <div className="mt-4">
                   {!examView ? <Schedule /> : <ExamSchedule />}
-                  <div className='d-grid'>
-                    <button className='btn btn-primary rounded-0 rounded-bottom' type='button' onClick={() => setExamView(!examView)}>
-                      {!examView ? 'View Exam Schedule' : 'View Course Schedule'}
+                  <div className="d-grid">
+                    <button
+                      className="btn btn-primary rounded-0 rounded-bottom"
+                      type="button"
+                      onClick={() => setExamView(!examView)}
+                    >
+                      {!examView
+                        ? "View Exam Schedule"
+                        : "View Course Schedule"}
                     </button>
                   </div>
                 </div>
